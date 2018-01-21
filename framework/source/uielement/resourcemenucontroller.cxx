d2da820a148cfc05847b3d18808a622d02c11779
Ben Vierck <me@xcud.com>
2018-01-20 23:13:10 -0600

    groom menus
    Change-Id: Icf72955cf79f24ce1890fea7c2e988adab6d7c2e

--- framework/source/uielement/resourcemenucontroller.cxx
+++ framework/source/uielement/resourcemenucontroller.cxx
@@ -428,11 +428,13 @@ void SaveAsMenuController::impl_setPopupMenu()
     if ( !pVCLPopupMenu )
         return;
 
+#ifndef VIEWONLY
     pVCLPopupMenu->InsertItem( ".uno:SaveAs", nullptr );
     pVCLPopupMenu->InsertItem( ".uno:ExportTo", nullptr );
     pVCLPopupMenu->InsertItem( ".uno:SaveAsTemplate", nullptr );
     pVCLPopupMenu->InsertSeparator();
     pVCLPopupMenu->InsertItem( ".uno:SaveAsRemote", nullptr );
+#endif
 }
 
 OUString SaveAsMenuController::getImplementationName()
